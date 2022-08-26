04c259c21fa0ce52974d1769f76913fe75f268c0
tohu-wabohu <tohu.palace@gmail.com>
2022-08-26 06:36:14 +0300

    up

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db/sqlite.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "OK"
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+@app.route('/recreate')
+def recreate():
+    db.drop_all()
+    db.create_all()
+    return 'Reacreation done'
+
+@app.route('/test')
+def test():
+    return 'test'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
